cfd2876468813082cba4bca347a342d4b3bef96f
Mosi8 <mauriziomosi8@gmail.com>
2022-01-13 17:03:23 +0100

    completo

---
+++ myscript.js
@@ -0,0 +1,8 @@
+let app = new Vue ({
+    el: '#blocco',
+    data: {
+        messaggio: 'Benvenuto in questa pagina.',
+        immagine: 'https://i.picsum.photos/id/76/600/600.jpg?hmac=Fcz-3lsR9rOjHqoqz-WWgiiWP5jDLWyHmmjjfqBqwrg',
+    }
+   }
+);
